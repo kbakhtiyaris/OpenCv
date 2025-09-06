81f7a644a966c223c3282dbc40ae7681d6e1fd28
kbakhtiyaris <09kbis@gmail.com>
2025-09-06 17:03:06 +0300

    after this projects

--- c2_Project1/c2_Project1/face_detection.cpp
+++ c2_Project1/c2_Project1/face_detection.cpp
@@ -8,17 +8,29 @@
 using namespace cv;
 using namespace std;
 //Mat imgGray, imgBlur, imgCanny, imgDil;
+Mat img, imgGray;
 
-
-void main() {
+void main()
+{
 
 
 	string path = "D:/c++/OpenCv/chapter1/Project1/Project1/resources/guy.jpg";
 	Mat img = imread(path);
+	
 	CascadeClassifier faceCascade;
-	faceCascade.load("D:/c++/OpenCv/c2_Project1/c2_Project1/haarcascade_frontalface_default");
-	if (faceCascade.empty()) { cout << "XML file not loaded" << endl; return; }
-	imshow("Image", img);
+	faceCascade.load("D:/c++/fine/opencv/data/haarcascades/haarcascade_frontalface_default.xml");
+	cvtColor(img, imgGray, COLOR_BGR2GRAY);
+
+	if (faceCascade.empty()) { cout << "XML file not loaded" << endl;  }
+
+	vector<Rect> faces;
+	faceCascade.detectMultiScale(imgGray, faces, 1.1, 10);
+	for (int i = 0; i < faces.size(); i++) 
+	{
+		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+	}
+
+	imshow("Image", imgGray);
 	waitKey(0);
 
 }
